491509261831a0b8c43e7c0b19d348752ff2adb6
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2026-03-04 23:00:46 +0200

    enhance: voicemeeter widget full support of any other audios devices
    now voicemeeter widget dynamically switches the volume interface at any time to the appropriate one in case uer used an audio device that is not a voicemeeter virtual audio device while using the widget. making this a super set of volume widget

--- src/core/utils/widgets/voicemeeter/service.py
+++ src/core/utils/widgets/voicemeeter/service.py
@@ -4,9 +4,8 @@ import threading
 from typing import override
 
 from pycaw.constants import DEVICE_STATE
-from pycaw.pycaw import AudioUtilities, EDataFlow
+from pycaw.pycaw import AudioDevice, AudioUtilities, EDataFlow
 
-from core.utils.utilities import is_process_running
 from core.utils.widgets.voicemeeter.VoicemeeterInterface import VoicemeeterApiLoginController, VoicemeeterInterface
 from core.utils.widgets.volume.service import AudioOutputService
 
@@ -66,20 +65,46 @@ class VoicemeeterService(AudioOutputService):
     # OVERRIDE
     @override
     def get_volume_interface(self):
-        """Get volume control interface."""
+        """Get volume control appropriate interface (either win api volume endpoints or voicemeeter interface)"""
+        if self._volume_interface is None:
+            self._volume_interface = self.try_get_volume_interfaces()
 
+        return self._volume_interface
+
+    def try_get_volume_interfaces(self):
+        "Try to  get the right interface for the default speakers (start with testing voicemeeter interface)"
+        speakers = self.get_speakers()
+        if speakers:
+            if self.is_voicemeeter_device(speakers):
+                self._volume_interface = self.try_voicemeeter_interface(speakers)
+            else:  # so the current speakers is not voicemeeter vritual device
+                self._volume_interface = self.try_win_api_volume_endpoint(speakers)
+
+        return self._volume_interface
+
+    def is_voicemeeter_device(self, audio_output_device: AudioDevice):
+        """Check if default audio output device is a voicemeeter interface compatible device"""
+        target = "voicemeeter"
+        # nieve: check if the name has voicemeeter in it -> target in device.FriendlyName.lower()  ?
+        # Bullet proof check!: check all props if have 'voicemeeter' keyword in it (props dict is not big -> 71 entries approx)
+        is_voicemeeter_device_used = False
+        device_properties_container = audio_output_device.properties
+        if device_properties_container is not None and isinstance(device_properties_container, dict):
+            for _, value in device_properties_container.items():
+                if isinstance(value, str) and target in value.lower():
+                    is_voicemeeter_device_used = True
+                    break
+
+        return is_voicemeeter_device_used
+
+    def try_voicemeeter_interface(self, voicemeeter_device: AudioDevice):
+        """Try using the voicemeeter interface class as the volume interface"""
         try:
-            if not (is_process_running("voicemeeterpro.exe") or is_process_running("voicemeeterpro_x64.exe")):
-                logging.warning(
-                    "'voicemeeterpro.exe' was not running before yasb starts. This MAY cause issues specially if using legacy vmcli.exe! -> launching voicemeeter via an internal command"
-                )
-
-            # maybe voicemeeterpro.exe init process includes a device change that triggers _on_device_change() in yasb
-            # if so it will call get_volume_interface() just in right time lazely when its ready!
             self._volume_interface = VoicemeeterInterface(
                 vmcli_exe_path=self.vmcli_exe_path,
                 main_output_bus=self.main_output_bus,
                 synced_outputs_count=self.synced_outputs_count,
+                virtual_speakers_obj=voicemeeter_device,
             )
 
         except Exception as e:
@@ -87,12 +112,13 @@ class VoicemeeterService(AudioOutputService):
                 f"from {inspect.stack()[0][3]}(): failed to use VoicemeeterInterface as the volume interface. Fallingback to default OS and yasb volume interface. err details: {e}"
             )
 
-            # fallback to default OS and yasb volume interface
-            speakers = self.get_speakers()
-            if speakers:
-                try:
-                    self._volume_interface = speakers.EndpointVolume
-                except:
-                    pass
+        return self._volume_interface
+
+    def try_win_api_volume_endpoint(self, speakers: AudioDevice):
+        """Try using the win api volume endpoint as the volume interface"""
+        try:
+            self._volume_interface = speakers.EndpointVolume
+        except Exception as e:
+            logging.warning(f"from {inspect.stack()[0][3]}(): failed to use Win API Volume Endpoint. Details: {e}")
 
         return self._volume_interface
